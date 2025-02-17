7b37d9209e5d7601da892346986833e289179fca
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-17 15:54:07 +0300

    Add Logo component to Sidebar for enhanced branding

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -1,7 +1,9 @@
+import Logo from "~/layouts/main/sidebar/logo/index.jsx";
+
 export default function Sidebar(){
     return(
-        <aside className="w-[275px] min-h-screen">
-            sidebar
+        <aside className="w-[275px] min-h-screen px-2">
+            <Logo />
         </aside>
     )
 }
