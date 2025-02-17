48c3901f91c164d29a1590dc32cebe795e7191cd
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-17 12:43:21 +0300

    Fix path resolution in Vite configuration by using fileURLToPath

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = new URL('', import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
